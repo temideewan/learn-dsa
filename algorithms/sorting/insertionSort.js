ea6deee70cef1001bac8d6cb0ffb6cf9401c9ad0
Temidayo <temideewan@gmail.com>
2021-09-25 21:25:10 +0100

    update the insertion sort

--- algorithms/sorting/insertionSort.js
+++ algorithms/sorting/insertionSort.js
@@ -1,11 +1,13 @@
 function insertionSort(arr) {
   for (let i = 1; i < arr.length; i++) {
     let currentVal = arr[i];
-    let j =i -1;
-    for (; j >= 0 && arr[j] > currentVal; j--) {
+    let j = i-1;
+    for(; j >= 0 && arr[j] > currentVal; j--) {
+      console.log(arr[j+1], arr[j]);
       arr[j + 1] = arr[j];
+
     }
-    arr[j + 1] = currentVal;
+    arr[ j + 1 ] = currentVal;
   }
 
   return arr;
