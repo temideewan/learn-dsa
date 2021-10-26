23ee239aab90f07459535d56f83f000a396ea293
Temidayo <temideewan@gmail.com>
2021-10-26 16:34:46 +0100

    implement radix sort

--- algorithms/intermediate_sorting/radix-sort.js
+++ algorithms/intermediate_sorting/radix-sort.js
@@ -1,10 +1,10 @@
-function getDigit(num, pos = 1){
-  let stringNum = num.toString();
-  return parseInt(stringNum[stringNum.length - 1 - pos]) || 0;
-}
-// function getDigitAgain(num, pos = 1){
-//   return Math.floor((Math.abs(num) / Math.pow(10, pos)))  % 10;
+// function getDigit(num, pos = 1){
+//   let stringNum = num.toString();
+//   return parseInt(stringNum[stringNum.length - 1 - pos]) || 0;
 // }
+function getDigitAgain(num, pos = 1){
+  return Math.floor((Math.abs(num) / Math.pow(10, pos)))  % 10;
+}
 
 function digitCount(num){
   if (num === 0) return 1;
@@ -21,4 +21,20 @@ function mostDigits(arr){
   return highest;
 }
 
-console.log(mostDigits([7323, 111111,23456,234567,213456909876543,654332213456789]));
+function radixSort(arr){
+  let longest = mostDigits(arr);
+  for(let i = 0; i < longest; i++){
+    let bucket = Array.from({length: 10}, () => []);
+    for (let j = 0; j < arr.length; j++){
+      let currentBucketIndex = getDigitAgain(arr[j], i);
+      bucket[currentBucketIndex].push(arr[j]);
+    }
+    console.log(bucket);
+    arr = [].concat(...bucket);
+
+    console.log(arr);
+  }
+  return arr;
+}
+
+console.log(radixSort([7323,111,2311,654567,]));
