a298d0b227101a77b7f47d112ad62ff44196b9a6
Temidayo <temideewan@gmail.com>
2021-12-01 17:54:23 +0100

    search for values in our binary tree

--- datastructures/BST/Binary_search_tree.js
+++ datastructures/BST/Binary_search_tree.js
@@ -1,5 +1,5 @@
 class Node {
-  constructor(value){
+  constructor(value) {
     this.value = value;
     this.left = null;
     this.right = null;
@@ -7,27 +7,27 @@ class Node {
 }
 
 class BinarySearchTree {
-  constructor(){
+  constructor() {
     this.root = null;
   }
 
-  insert(value){
+  insert(value) {
     let newNode = new Node(value);
-    if(this.root === null){
+    if (this.root === null) {
       this.root = newNode;
       return this;
     }
     let current = this.root;
-    while(true){
-      if(value === current.value) return undefined;
-      if(value < current.value){
-        if(current.left === null){
+    while (true) {
+      if (value === current.value) return undefined;
+      if (value < current.value) {
+        if (current.left === null) {
           current.left = newNode;
           return this;
         }
         current = current.left;
       } else {
-        if(current.right === null){
+        if (current.right === null) {
           current.right = newNode;
           return this;
         }
@@ -35,8 +35,39 @@ class BinarySearchTree {
       }
     }
   }
-}
 
+  find(value) {
+    if (this.root === null) return false;
+    let current = this.root;
+    let found = false;
+    while (current && !found) {
+      if (value < current.value) {
+        current = current.left;
+      } else if (value > current.value) {
+        current = current.right;
+      } else {
+        found = true;
+      }
+    }
+    if(!found) return undefined;
+    return current;
+  }
+  contains(value) {
+    if (this.root === null) return false;
+    let current = this.root;
+    let found = false;
+    while (current && !found) {
+      if (value < current.value) {
+        current = current.left;
+      } else if (value > current.value) {
+        current = current.right;
+      } else {
+       return true;
+      }
+    }
+    return false;
+  }
+}
 
 const tree = new BinarySearchTree();
 tree.root = new Node(10);
